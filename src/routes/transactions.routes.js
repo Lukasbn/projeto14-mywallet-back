123f4951f2aa8d778f528591111fdbb35b4731d4
LucasBento <lubentoneves@gmail.com>
2023-04-25 07:44:06 -0300

    alter port

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express"
 import { gethome, postNewTransaction } from "../controllers/transactions.controllers.js"
-import { authorizationValidation } from "../middlewares/autorization.middleware.js"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js"
 import { newTransitionSchema } from "../schemas/transactions.schemas.js"
 
 const transactionsRouter = Router()
 
-transactionsRouter.post("/nova-transacao/:tipo",validateSchema(newTransitionSchema),authorizationValidation,postNewTransaction)
-transactionsRouter.get("/home", authorizationValidation,gethome)
+transactionsRouter.post("/nova-transacao/:tipo",validateSchema(newTransitionSchema),postNewTransaction)
+transactionsRouter.get("/home",gethome)
 
 export default transactionsRouter
